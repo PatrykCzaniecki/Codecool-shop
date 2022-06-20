f15bf301da680b0bf89310f2b7117bd9c01189a3
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-06-20 13:29:20 +0200

    add two controllers for payment and order

---
+++ src/Codecool.CodecoolShop/Controllers/OrderConfirmationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    [Route("orderconfirmation")]
+    public class OrderConfirmationController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index(IFormCollection collection)
+        {
+            IOrderDao orderDataStore = OrderDaoMemory.GetInstance();
+            Order order = orderDataStore.Get(1);
+
+            PaymentInfo paymentInfo = new PaymentInfo();
+            paymentInfo.NameOnCard = collection["cardname"];
+            paymentInfo.CardNumber = collection["cardnumber"];
+            paymentInfo.ExpMonth = collection["expmonth"];
+            paymentInfo.ExpYear = collection["expyear"];
+            paymentInfo.CVV = collection["cvv"];
+
+            order.PaymentInfo = paymentInfo;
+
+            SessionHelper.SaveToJsonFile(order, order.Id);
+            ViewBag.order = order;
+
+            return View();
+        }
+    }
+}
---
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    [Route("payment")]
+    public class PaymentController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index(IFormCollection collection)
+        {
+            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+
+            IOrderDao orderDataStore = OrderDaoMemory.GetInstance();
+
+            Order order = new Order
+            {
+                ItemCollection = cart,
+                PaymentInfo = new PaymentInfo(),
+                Total = cart.Sum(item => item.Product.DefaultPrice * item.Quantity),
+                FullName = collection["fullname"],
+                Email = collection["email"],
+                Address = collection["address"],
+                City = collection["city"],
+                State = collection["state"],
+                Zip = collection["zip"],
+                FullNameShipping = collection["fullnameShipping"],
+                EmailShipping = collection["emailShipping"],
+                AddressShipping = collection["addressShipping"],
+                CityShipping = collection["cityShipping"],
+                StateShipping = collection["stateShipping"],
+                ZipShipping = collection["zipShipping"]
+            };
+
+            orderDataStore.Add(order);
+
+            return View();
+        }
+    }
+}
