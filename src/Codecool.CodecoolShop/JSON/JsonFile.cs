b0d09fa0fbfcb092c035af0055f9faeaed8f61d5
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-06-21 11:17:54 +0200

    saving order to json

---
+++ src/Codecool.CodecoolShop/JSON/JsonFile.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text.Json;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.JSON;
+
+public static class JsonFile
+{
+    public static void SaveToJsonFile(Order value, int orderNumber)
+    {
+        var options = new JsonSerializerOptions {WriteIndented = true};
+        var jsonString = JsonSerializer.Serialize(value, options);
+        jsonString += JsonSerializer.Serialize(value.ItemCollection, options);
+        File.WriteAllText($"../savedOrder{orderNumber}.json", jsonString);
+    }
+}
