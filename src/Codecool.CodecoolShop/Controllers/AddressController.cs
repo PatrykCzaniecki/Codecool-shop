2e03b2b5cc6640b1270480a69acd25362fbc1f6a
Leon Stawecki <stawecki.media@gmail.com>
2022-06-21 14:59:33 +0200

    Aded adrress

--- src/Codecool.CodecoolShop/Controllers/AddressController.cs
+++ src/Codecool.CodecoolShop/Controllers/AddressController.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Diagnostics;
+using Codecool.CodecoolShop.Daos;
 using Codecool.CodecoolShop.Daos.Implementations;
 using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 
-namespace Codecool.CodecoolShop.Controllers
-{
-    public class AdressController : Controller
+
+namespace Codecool.CodecoolShop.Controllers;
+
+    public class AddressController : Controller
     {
-        private readonly ILogger<AdressController> _logger;
-
-        public AdressController(ILogger<AdressController> logger)
-        {
-            _logger = logger;
-        }
-        public Address address;
-
         public IActionResult Index()
         {
-            var cartDaoMemory = CartDaoMemory.GetInstance();
+            
             return View();
         }
 
-        public IActionResult Submit(object sender, EventArgs e)
+    [HttpPost]
+    public IActionResult Index(Address addressGet)
+    {
+        var address = AddressDaoMemory.GetInstance();
+        address.adress = addressGet;
+
+        return RedirectToAction();
+    }
+
+    public IActionResult Submit(IFormCollection collection)
         {
             return View("Index");
         }
@@ -35,5 +39,12 @@ namespace Codecool.CodecoolShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
 }
+
--- src/Codecool.CodecoolShop/Models/Address.cs
+++ src/Codecool.CodecoolShop/Models/Address.cs
@@ -1,18 +1,19 @@
-﻿namespace Codecool.CodecoolShop.Models;
-
-public class Address
+﻿namespace Codecool.CodecoolShop.Models
 {
-    public string FullName { get; set; }
+    public class Address
+    {
+        public string FullName { get; set; }
 
-    public string Phone { get; set; }
+        public string Phone { get; set; }
 
-    public string Email { get; set; }
+        public string Email { get; set; }
 
-    public string Street { get; set; }
+        public string Street { get; set; }
 
-    public string Country { get; set; }
+        public string Country { get; set; }
 
-    public string City { get; set; }
+        public string City { get; set; }
 
-    public string Zip { get; set; }
+        public string Zip { get; set; }
+    }
 }
