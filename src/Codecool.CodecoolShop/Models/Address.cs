e1f4a803fb65d28dc866eb1138da0fd693259704
Kamil Kornek <kamil.kornek96@gmail.com>
2022-06-22 14:18:31 +0200

    basic validation added

--- src/Codecool.CodecoolShop/Models/Address.cs
+++ src/Codecool.CodecoolShop/Models/Address.cs
@@ -1,18 +1,21 @@
-﻿namespace Codecool.CodecoolShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Codecool.CodecoolShop.Models;
 
 public class Address
 {
+    [Required]
     public string FullName { get; set; }
-
+    [Required]
     public string Phone { get; set; }
-
+    [Required]
     public string Email { get; set; }
-
+    [Required]
     public string Street { get; set; }
-
+    [Required]
     public string Country { get; set; }
-
+    [Required]
     public string City { get; set; }
-
+    [Required]
     public string Zip { get; set; }
 }
--- src/Codecool.CodecoolShop/Models/PaymentInfo.cs
+++ src/Codecool.CodecoolShop/Models/PaymentInfo.cs
@@ -1,14 +1,17 @@
-﻿namespace Codecool.CodecoolShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Codecool.CodecoolShop.Models;
 
 public class PaymentInfo
 {
+    [Required]
     public string NameOnCard { get; set; }
-
+    [Required]
     public string CardNumber { get; set; }
-
+    [Required]
     public string ExpMonth { get; set; }
-
+    [Required]
     public string ExpYear { get; set; }
-
+    [Required]
     public string CVV { get; set; }
 }
