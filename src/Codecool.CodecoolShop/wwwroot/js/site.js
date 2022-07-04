717e2d3fd454cb69a4726f5762bff7f96ed1f6a5
Kamil Kornek <kamil.kornek96@gmail.com>
2022-07-04 09:54:36 +0200

    .

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-<<<<<<< HEAD
-// Write your JavaScript code.
-=======
-// Write your JavaScript code.
->>>>>>> r1remote/development
