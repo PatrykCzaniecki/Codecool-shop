842392b7dc3e6c6a196a96de429cde3940819fcb
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-07-05 11:02:38 +0200

    signup users not complete

--- src/Codecool.CodecoolShop/Models/SignUp.cs
+++ src/Codecool.CodecoolShop/Models/SignUp.cs
@@ -4,12 +4,11 @@ namespace Codecool.CodecoolShop.Models;
 
 public class SignUp
 {
-    [Required] public string FullName { get; set; }
+    public string FullName { get; set; }
 
-    [Required]
-    [RegularExpression(
-        "\\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\\Z")]
     public string Email { get; set; }
 
-    [Required] public string Password { get; set; }
+    public string Password { get; set; }
+
+    public string ConfirmPassword { get; set; }
 }
